38410f36c2711746872146d429bfad88e2ed1af9
Matej Gera <matejgera@gmail.com>
2022-06-24 16:04:40 +0200

    Change Dockerfile path to be absolute
    Signed-off-by: Matej Gera <matejgera@gmail.com>

--- tests/integration_tests/build_deploy.sh
+++ tests/integration_tests/build_deploy.sh
@@ -2,10 +2,13 @@
 
 set -exv
 
+# We need to find absolute path since CI is running the script from repo's root directory.
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 IMAGE="quay.io/app-sre/rhobs-e2e"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
-docker build -t "${IMAGE}:${IMAGE_TAG}" .
+docker build -t "${IMAGE}:${IMAGE_TAG}" -f "${ABSOLUTE_PATH}/Dockerfile" .
 
 if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
     DOCKER_CONF="$PWD/.docker"

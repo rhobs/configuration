de298a80c9ec18926e37a08968ef1a5b836cb89b
RaphaelBut <rbut@redhat.com>
2023-05-24 11:10:59 +0200

    Add readonly account for hypershift metrics

--- configuration/observatorium/rbac.go
+++ configuration/observatorium/rbac.go
@@ -223,6 +223,17 @@ func GenerateRBAC(gen *mimic.Generator) {
 		envs:    []env{productionEnv},
 	})
 
+	// hypershift
+	// Special request of extra read account.
+	// Ref: https://issues.redhat.com/browse/OHSS-22439
+	attachBinding(&obsRBAC, bindingOpts{
+		name:    "observatorium-hypershift-platform-read",
+		tenant:  hypershiftTenant,
+		signals: []signal{metricsSignal},
+		perms:   []rbac.Permission{rbac.Read}, // Read only.
+		envs:    []env{productionEnv},
+	})
+
 	// hypershift staging
 	// observatorium-hypershift-platform-staging is the only tenant that does not
 	// follow conventions, due to them being present in an unique environment alongside
@@ -232,6 +243,18 @@ func GenerateRBAC(gen *mimic.Generator) {
 		tenant:              hypershiftStagingTenant,
 		signals:             []signal{metricsSignal},
 		perms:               []rbac.Permission{rbac.Write, rbac.Read},
+		envs:                []env{productionEnv},
+		skipConventionCheck: true,
+	})
+
+	// hypershift staging
+	// Special request of extra read account.
+	// Ref: https://issues.redhat.com/browse/OHSS-22439
+	attachBinding(&obsRBAC, bindingOpts{
+		name:                "observatorium-hypershift-platform-staging-read",
+		tenant:              hypershiftStagingTenant,
+		signals:             []signal{metricsSignal},
+		perms:               []rbac.Permission{rbac.Read}, // Read only.
 		envs:                []env{productionEnv},
 		skipConventionCheck: true,
 	})

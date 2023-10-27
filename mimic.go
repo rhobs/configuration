121210f4cd8f47e04c4ac2ca7167e08b5a82aa96
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2023-10-27 16:29:42 +0200

    fix
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- mimic.go
+++ mimic.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/bwplotka/mimic"
 	cfgobservatorium "github.com/rhobs/configuration/configuration/observatorium"
-	services "github.com/rhobs/configuration/services_go"
 )
 
 func main() {
@@ -16,6 +15,6 @@ func main() {
 	cfgobservatorium.GenerateRBAC(gen.With(".tmp", "tenants"))
 
 	// Generate the manifests for all observatorium instances.
-	services.Generate(gen.With("services"))
+	// services.Generate(gen.With("services"))
 
 }

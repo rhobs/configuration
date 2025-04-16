ca141ec191ab6641cd6e360aa6c0fd1c89ab067f
Philip Gough <philip.p.gough@gmail.com>
2025-04-16 11:15:56 +0100

    Remove outdated generated mimic code and make targets (#853)

--- mimic.go
+++ mimic.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/bwplotka/mimic"
 	cfgobservatorium "github.com/rhobs/configuration/configuration/observatorium"
-	services "github.com/rhobs/configuration/services_go"
 )
 
 func main() {
@@ -14,8 +13,5 @@ func main() {
 	cfgobservatorium.GenSLO(gen.With("observability", "prometheusrules", "pyrra"), gen.With("observability", "prometheusrules"))
 
 	cfgobservatorium.GenerateRBACFile(gen.With(".tmp", "tenants"))
-
-	// Generate the manifests for all observatorium instances.
-	services.Generate(gen.With("services"))
 
 }

014bacd1fc6455aa38ea9040d7ab37476860d3de
Philip Gough <philip.p.gough@gmail.com>
2025-08-14 14:16:26 +0100

    loki: Use env vars to deploy offical RH builds of loki (#967)

--- magefiles/loki-operator.go
+++ magefiles/loki-operator.go
@@ -135,6 +135,12 @@ func NewControllerManagerDeployment(namespace string) *appsv1.Deployment {
 						ImagePullPolicy: corev1.PullIfNotPresent,
 						Command:         []string{"/manager"},
 						Ports:           []corev1.ContainerPort{{Name: "metrics", ContainerPort: 8080}},
+						Env: []corev1.EnvVar{
+							{Name: "RELATED_IMAGE_LOKI", Value: "registry.redhat.io/openshift-logging/logging-loki-rhel9@sha256:14f37195a4957e3848690d0ffe5422be55f7599b30dfe1ee0f97eb1118a10a51"},
+							{Name: "RELATED_IMAGE_GATEWAY", Value: "registry.redhat.io/openshift-logging/lokistack-gateway-rhel9@sha256:710a1a5e486de5724469e55f29e9ff3f6cbef8cd4b2d21dfe254ede2b953c150"},
+							{Name: "RELATED_IMAGE_OPA", Value: "registry.redhat.io/openshift-logging/opa-openshift-rhel9@sha256:06602373b99d694a83b845e9cc6746a4f37a7a6ca6943356fea6ee6dbbccda35"},
+							{Name: "OPERATOR_CONDITION_NAME", Value: "loki-operator.v6.3.0"},
+						},
 					}},
 					TerminationGracePeriodSeconds: ptr.To(int64(10)),
 				},
